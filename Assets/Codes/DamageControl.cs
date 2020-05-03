4d176d8fb9d62e8d1071b2d3cde30a57ed6e95dc
Quadrado04 <Quadrad04@users.noreply.github.com>
2020-05-03 18:34:56 -0300

    Initial commit

---
+++ Assets/Codes/DamageControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DamageControl : MonoBehaviour
+{
+    public int lifes = 3;
+    public IAWalk iawalk;
+	public SkinnedMeshRenderer render;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage()
+    {
+        iawalk.currentState = IAWalk.IaState.Dying;
+		StartCoroutine(Blink());
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Projectiles"))
+        {
+            lifes--;
+            iawalk.currentState = IAWalk.IaState.Damage;
+        }
+        if (lifes < 0)
+        {
+            iawalk.currentState = IAWalk.IaState.Dying;
+          
+        }
+    }
+	IEnumerator Blink()
+	{
+		int blinks = 6;
+		while (blinks > 0)
+		{
+			render.enabled = !render.enabled;
+			yield return new WaitForSeconds(0.1f);
+			blinks--;
+		}
+	}
+
+}
---
+++ Assets/Codes/TrdWalk.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrdWalk : MonoBehaviour
+{
+    public enum States
+    {
+        idle,
+        walk,
+        jump,
+        die,
+        attack,
+		Hit,
+    }
+    public States state;
+    public Animator anim;
+    public Rigidbody rdb;
+
+    public Vector3 move { get; private set; }
+    public float movforce=100;
+
+    Vector3 direction;
+
+    GameObject referenceObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Idle());
+
+        referenceObject=Camera.main.GetComponent<trdCam>().GetRefereceObject();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //criacao de vetor de movimento local
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = referenceObject.transform.TransformDirection(move);
+
+        //girar pra direcao das teclas
+        if (move.magnitude > 0)
+        {
+            direction = move;
+        }
+        transform.forward =Vector3.Lerp(transform.forward,direction,Time.fixedDeltaTime*20);
+
+       
+        //reduz a força de movimento de acordo com a velocidade pra ter muita força de saida mas pouca velocidade. 
+        rdb.AddForce(move * (movforce/(rdb.velocity.magnitude+1)));
+
+       
+    }
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Idle()
+    {
+        //equivalente ao Start 
+        state = States.idle;
+
+        
+        //
+        while (state == States.idle)
+        {
+            //equivalente ao update
+
+            anim.SetFloat("Velocity", 0);
+            if (rdb.velocity.magnitude > 0.1f)
+            {
+                StartCoroutine(Walk());
+            }
+            //
+            yield return new WaitForEndOfFrame();
+        }
+        //saida do estado
+    }
+
+    IEnumerator Walk()
+    {
+        //equivalente ao Start 
+        state = States.walk;
+
+
+        //
+        while (state == States.walk)
+        {
+            //equivalente ao update
+
+            anim.SetFloat("Velocity", rdb.velocity.magnitude);
+            if (rdb.velocity.magnitude < 0.1f)
+            {
+                StartCoroutine(Idle());
+            }
+            //
+            yield return new WaitForEndOfFrame();
+        }
+        //saida do estado
+    }
+
+
+    IEnumerator Attack()
+    {
+        //equivalente ao Start 
+        state = States.attack;
+        anim.SetTrigger("Attack");
+        yield return new WaitForSeconds(.5f);
+        //saida do estado
+        StartCoroutine(Idle());
+    }
+	IEnumerator Hit()
+	{
+		//equivalente ao Start 
+		state = States.Hit;
+		anim.SetTrigger("Hit");
+
+		yield return new WaitForSeconds(.5f);
+
+		StartCoroutine(Idle());	
+	}
+	public void Damage()
+	{
+		StartCoroutine(Hit());
+	}
+}
