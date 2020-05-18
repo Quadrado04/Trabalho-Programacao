9bab1db4cdcc1ad7f8cfc197dedb1ea706f6ca56
Quadrado04 <Quadrad04@users.noreply.github.com>
2020-05-18 12:58:50 -0300

    Revert "Revert "Revert "Jump"""
    This reverts commit 553fe6f5c51630fc79be941e196ef879066d2bec.

--- Assets/Codes/ItemGrabber.cs
+++ Assets/Codes/ItemGrabber.cs
@@ -17,13 +17,15 @@ public class ItemGrabber : MonoBehaviour
                 weaponOnHand.GetComponent<Rigidbody>().isKinematic = false;
                 weaponOnHand.transform.Translate(-transform.up);
                 weaponOnHand.layer = 0;
-            }
+				other.GetComponent<PhisicalWeapon>().TakeDmg = false;
+			}
 
             weaponOnHand = other.gameObject;
             other.transform.parent = handposition; //coloca como filho da mao
             other.transform.localPosition = Vector3.zero;//vai pra posicao da mao
             weaponOnHand.GetComponent<Rigidbody>().isKinematic = true;//desativa o rigidbody
             other.transform.localRotation = Quaternion.identity;//reseta a rotacao
+			other.GetComponent<PhisicalWeapon>().TakeDmg = true;
             other.transform.gameObject.layer = transform.gameObject.layer;
 
         }
