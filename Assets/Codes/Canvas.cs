cd784b386e9696848c16c6d7e544fe36daa6fc40
Quadrado04 <Quadrad04@users.noreply.github.com>
2020-06-01 13:22:04 -0300

    Finalizado

---
+++ Assets/Codes/Canvas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Canvas : MonoBehaviour
+{
+	public GameObject inimigos;
+	public float time = 5;
+	public Text lifes;
+	public Text enemies;
+	private TrdWalk player;
+	private IAController enemy;
+    void Start()
+    {
+		StartCoroutine(TextTime());
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<TrdWalk>();
+		enemy = GameObject.FindObjectOfType<IAController>();
+		
+    }
+	private void Update()
+	{
+		lifes.text = "Vidas: " + player.lifes;
+		enemies.text = "Inimigos: " + enemy.inimigos;
+	}
+	IEnumerator TextTime()
+	{
+		inimigos.SetActive(true);
+		yield return new WaitForSeconds(time);
+		inimigos.SetActive(false);
+	}
+}
